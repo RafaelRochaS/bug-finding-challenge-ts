60a56956a5f24c91091a31939d23d669c4f27a5c
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2025-08-26 16:57:02 -0300

    fix: bank account tests

--- bankAccount.ts
+++ bankAccount.ts
@@ -7,8 +7,10 @@ class BankAccount {
     this.balance += amount
   }
 
-  withdraw(amount: number) {
+  withdraw(amount: number): number {
     this.balance -= amount
+
+    return amount
   }
 
   getBalance() {
@@ -18,11 +20,25 @@ class BankAccount {
 
 const account = new BankAccount()
 
-console.log("Depositando...")
+console.log("Depositando 1000...")
+account.deposit(1000)
+
+assert.strictEqual(account.getBalance(), 1000)
+
+console.log("Sacando 200...")
+let withdrawn = account.withdraw(200)
+assert.strictEqual(withdrawn, 200)
+
+assert.strictEqual(account.getBalance(), 800)
+
+console.log("Depositando 100...")
 account.deposit(100)
 
-console.log("Sacando...")
-account.withdraw(200)
+assert.strictEqual(account.getBalance(), 900)
+
+console.log("Sacando 1100...")
+withdrawn = account.withdraw(1100)
+assert.strictEqual(withdrawn, 1000)
 
 assert.strictEqual(account.getBalance(), 0)
 console.log("Passou!")
