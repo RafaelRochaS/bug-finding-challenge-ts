e8fa4f9a585372e9a6d4bbd7314d86516eb016d4
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2025-08-26 17:02:57 -0300

    fix: bank account tests

--- bankAccount.ts
+++ bankAccount.ts
@@ -1,18 +1,25 @@
 import assert from "assert"
 
+/** 
+ *  Classe representando uma conta em um banco.
+ *  Atenção: o banco não permite saldo negativo!
+ * */
 class BankAccount {
   private balance: number = 0
 
+  // Deposita um valor na conta
   deposit(amount: number) {
     this.balance += amount
   }
 
+  // Remove (saca) um valor da conta
   withdraw(amount: number): number {
     this.balance -= amount
 
     return amount
   }
 
+  // Retorna o saldo atual da conta
   getBalance() {
     return this.balance
   }
@@ -38,7 +45,7 @@ assert.strictEqual(account.getBalance(), 900)
 
 console.log("Sacando 1100...")
 withdrawn = account.withdraw(1100)
-assert.strictEqual(withdrawn, 1000)
+assert.strictEqual(withdrawn, 900)
 
 assert.strictEqual(account.getBalance(), 0)
 console.log("Passou!")
